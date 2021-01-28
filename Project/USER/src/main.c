9e25450066f5313cc21fe5a0689b9c1c0c669586
TITH <1023237464@qq.com>
2021-01-28 14:20:09 +0800

    将Libraries添加进Git，精简main.c

--- Project/USER/src/main.c
+++ Project/USER/src/main.c
@@ -1,41 +1,7 @@
-/*********************************************************************************************************************
-* COPYRIGHT NOTICE
-* Copyright (c) 2019,逐飞科技
-* All rights reserved.
-* 技术讨论QQ群：一群：179029047(已满)  二群：244861897
-*
-* 以下所有内容版权均属逐飞科技所有，未经允许不得用于商业用途，
-* 欢迎各位使用并传播本程序，修改内容时必须保留逐飞科技的版权声明。
-*
-* @file				main
-* @company			成都逐飞科技有限公司
-* @author			逐飞科技(QQ3184284598)
-* @version			查看doc内version文件 版本说明
-* @Software			IAR 8.3 or MDK 5.24
-* @Target core		MM32SPIN2XPs
-* @Taobao			https://seekfree.taobao.com/
-* @date				2020-11-23
-********************************************************************************************************************/
-
 #include "headfile.h"
-#include "MotorVolt.h"
+#include "MotorVolt.h"//
 #include "PID_SpeedControl.h"
 #include <stdlib.h>
-
-
-// *************************** 例程说明 ***************************
-// 
-// 测试需要准备逐飞科技MM32SPIIN27PS核心板一块
-// 
-// 调试下载需要准备逐飞科技CMSIS-DAP或Jlink调试下载器一个
-// 
-// 本例程是个空工程 用来给同学们移植使用
-// 
-// 打开新的工程或者工程移动了位置务必执行以下操作
-// 第一步 关闭上面所有打开的文件
-// 第二步 project  clean  等待下方进度条走完
-// 
-// *************************** 例程说明 ***************************
 
 // **************************** 宏定义 ****************************
 
@@ -44,23 +10,26 @@ const double PI = 3.1415926535897932384626433832795;
 // **************************** 宏定义 ****************************
 
 // **************************** 变量定义 ****************************
-uint16 loop_count = 0;
-uint8 io_input_state = 0;
 
 uint32_t dt = 0;//两次循环的间隔时间（微秒）
 uint32_t total_us = 0;//总时间
 
 double acc_angle_yz = 0;//y,z轴方向加速度的角度
-
 const double bal_acc_angle_yz = 0.81;//平衡时的角度
-
 double angle_yz_err = 0;
 
-uint16 send_index = 0;
 
 // **************************** 变量定义 ****************************
 
 // **************************** 代码区域 ****************************
+
+void PrintData();
+void Update_Gyro_Acc();
+char* getrxs();
+void Clear_rx_buffer();
+void GetSpeedFromRX();
+int IsCharNum(char*);
+
 int main(void)
 {
 	board_init(true);																// 初始化 debug 输出串口
@@ -91,8 +60,6 @@ int main(void)
 			exp_Speed1 = -50;
 			exp_Speed2 = -50;
 		} */
-
-
 
 		GetSpeedFromRX();
 		
